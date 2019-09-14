66488626db16517863d68b48fa682d42a3baae05
Peter <petran437@gmail.com>
2019-09-14 17:59:09 -0400

    Working camera

--- MemoryEye/MemoryEye/HomeViewController.swift
+++ MemoryEye/MemoryEye/HomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +16,27 @@ class HomeViewController: UIViewController {
     }
     
     @IBAction func pushedMemoryEyeButton(_ sender: Any) {
-        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "lol") as?
-            ViewController
-        {
-            present(vc, animated: true, completion: nil)
-        }
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+//        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "lol") as?
+//            ViewController
+//        {
+//            present(vc, animated: true, completion: nil)
+//        }
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        // print out the image size as a test
+        print(image.size)
+    }
 }
