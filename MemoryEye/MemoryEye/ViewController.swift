3c0832de3944a892c32cf0464e162733d11fab5c
Elaine Xiao <eyxiao@mit.edu>
2019-09-15 04:57:48 -0400

    Small edits to ViewController

--- MemoryEye/MemoryEye/ViewController.swift
+++ MemoryEye/MemoryEye/ViewController.swift
@@ -11,23 +11,26 @@ import FirebaseStorage
 import Firebase
 
 class ViewController: UIViewController {
-
-    let db = Database.database().reference()
-    let storage = Storage.storage().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
+
+class Memories {
+    let db = Database.database().reference()
+    let storage = Storage.storage().reference()
     
+    // Helper function to add images to Firebase Storage, then retrieve its URL
+    // Params: img (UIImage type)
+    // Returns: url of the image
     func uploadImage(img: UIImage) -> String {
         let imgPNG = img.pngData()!
         let imgRef = storage.child("name.jpg")
         
         // upload to firebase storage, then retrieve url
-        
         var finalURL = "temp"
-        
         let uploadTask = imgRef.putData(imgPNG, metadata: nil) { (metadata, error) in
             guard let metadata = metadata else {
                 // Uh-oh, an error occurred!
@@ -45,29 +48,49 @@ class ViewController: UIViewController {
         return finalURL
     }
     
+    func setModelNumber(num: String) {
+        db.child("memories").child("modelNum").setValue(num)
+    }
     
-    func storeNewMemory(loc: String, date: String, text: String, imgs: Array<UIImage>) {
+    func getModelNumber(num: String) -> String {
+        return db.child("memories").child("modelNum").key!
+    }
+    
+    // Stores information into Firebase Realtime Database.
+    // Params: loc (location of memory)
+    //         date (date of memory)
+    //         text (description of memory)
+    //         name (name of memory)
+    //         imgs (array of UIImages corresponding to the memory)
+    // Returns: Nothing, just adds stuff to database and updates Custom Vision model.
+    func storeNewMemory(loc: String, date: String, text: String, name: String, imgs: Array<UIImage>) {
         let tag = db.child("memories").childByAutoId().key
         
+        // Store location, date, text, and name into database
         db.child("memories").child(tag!).setValue(["loc": loc,
                                                    "date": date,
-                                                   "text": text])
-        
+                                                   "text": text,
+                                                   "name": name])
         var imgURLArray: Array<String> = Array()
         for img in imgs {
             let url = uploadImage(img: img)
             imgURLArray.append(url)
         }
         
-        // use Elizabeth's stuff to get tagID (input imgURLArray)
+        tagID = ComputerVision.addImages(imgURLArray, tag)
         
+        // Store img urls and tagID in database
+        db.child("memories").child(tag!).setValue(["imgs": imgURLArray,
+                                                   "tagID": tagID])
     }
     
-    // Matches the input image to an object in the database. Then, sends the info from the database related to the object
-    // to frontend.
+    // Matches the input image to an object in the database and sends the info from the database related to the object to frontend.
+    // Params: img (type UIImage)
+    // Returns: info (date, location, time) corresponding to the object that the Custom Vision model predicts the image matches.
     func rememberMemory(img: UIImage) {
+        let imgURL = uploadImage(img: img)
+        let probs = ComputerVision.classify(imgURL)
         
+        // finish method based on what Elizabeth returns
     }
-    
 }
-
