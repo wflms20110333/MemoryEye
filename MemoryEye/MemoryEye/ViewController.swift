0f24cb62f1c6fed9f9cf850bbcbfe69a90b21a4b
Peter T Tran <petpanda@mac-56-129-08.mit.edu>
2019-09-15 02:14:35 -0400

    keyboard scrolling

--- MemoryEye/MemoryEye/ViewController.swift
+++ MemoryEye/MemoryEye/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var addSign3: UIButton!
     @IBOutlet weak var addSign4: UIButton!
     
+    @IBOutlet weak var bottomScrollConstraint: NSLayoutConstraint!
+    
     var imagePicker = UIImagePickerController()
     var count: Int = 0
 
@@ -41,6 +43,36 @@ class ViewController: UIViewController {
         addSign2.layer.cornerRadius = 11.0
         addSign3.layer.cornerRadius = 11.0
         addSign4.layer.cornerRadius = 11.0
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardNotification(notification:)),
+                                               name: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame?.origin.y ?? 0
+            let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
+            if endFrameY >= UIScreen.main.bounds.size.height {
+                self.bottomScrollConstraint?.constant = 0.0
+            } else {
+                self.bottomScrollConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
     }
 
     @IBAction func addImage1(_ sender: Any) {
